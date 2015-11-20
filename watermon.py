cf74ed7f3735f9a833c02834dc715f01d5c678b7
Kevin Anthony <kevin@anthonynet.org>
2015-11-20 01:29:46 -0400

    Cleaning up

--- watermon.py
+++ watermon.py
@@ -1,40 +1,55 @@
 #!/usr/bin/python3
+
+# Water Monitor
+#
+# Reads a HMC5883L sensor to detect a magnet spinning inside a water meter.
+# Reverses to field strength on the y axis are counted to detect water is flowing.
+# Liters in a period are uploaded to grovestreams for graphing.
+
 import time
 import datetime
 import http.client
 import random
 import urllib
 
+#from daemonize import Daemonize
 from i2clibraries import i2c_hmc5883l
 
-def arduino_map(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+pid = "/tmp/watermon.pid"
 
+# Change 1 to different i2c if needed
 hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
 hmc5883l.setContinuousMode()
 hmc5883l.setDeclination(9,54)
 
+# API Key for Grovestreams (should have write and auto create permissions)
 grovestreams_apikey = 'XXXXXXXXXXXXXX'
 
+# Name of component at Grovestreams
 component_id = "Water"
-base_url = '/api/feed?'
 
+base_url = '/api/feed?'
 conn = http.client.HTTPConnection('www.grovestreams.com')
 
-old_val = 0;
-new_val = 0;
-crossings = 0;
+# How often to publish
+publish_delay = 60
+# Amount of water per count
 liter_per_count = 0.017
 
-publish_delay = 60
+old_val = 0
+new_val = 0
+crossings = 0
 
 last_publish = time.time()
 
-while 1:
+print('Starting collection..')
+
+while True:
     now = time.time()
 
     (x, y, z) = hmc5883l.getAxes()
     old_val = new_val
+    # Only using the y axis due to orientation on meter
     new_val = y
     changed = (old_val < 0 and new_val > 0) or (old_val > 0 and new_val < 0)
     if(changed):
